a8c65354ab9cb22f774fea0a852dd5a5e8054e47
Madu-f0r0 <duherbert@gmail.com>
2024-01-02 21:39:55 +0100

    API 2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,6 +5,7 @@
 """
 
 if __name__ == "__main__":
+    import csv
     import json
     from sys import argv
     import urllib.request
@@ -16,17 +17,16 @@ if __name__ == "__main__":
     with urllib.request.urlopen(user_url) as response:
         user_details_json = response.read()
         user_details_python = json.loads(user_details_json)
-        employee_name = user_details_python.get("name")
+        username = user_details_python.get("username")
+        user_id = user_details_python.get("id")
 
     todos_url = f"{todos}?userId={argv[1]}"
     with urllib.request.urlopen(todos_url) as response:
         all_tasks_json = response.read()
         all_tasks = json.loads(all_tasks_json)
 
-        tasks_done = [task for task in all_tasks if task.get("completed")]
-
-        print("Employee {} is done with tasks({}/{}):".format(
-              employee_name, len(tasks_done), len(all_tasks)))
-
-        for task in tasks_done:
-            print("\t {}".format(task.get("title")))
+    with open("{}.csv".format(user_id), "w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, str(t.get("completed")), str(t.get("title"))]
+            ) for t in all_tasks]

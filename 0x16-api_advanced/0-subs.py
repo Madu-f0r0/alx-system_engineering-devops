5cf7fbdd2277225929c799ad3bc3927f5f4b80ee
Madu-f0r0 <duherbert@gmail.com>
2024-01-16 15:52:02 +0100

    Script to query reddit api for subreddits subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module contains a function that returns the number of
+subscribers for a given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Returns the number of subscribers for  given subreddit.
+
+    subreddit: (string) - the subreddit to be queried
+
+    Return: 0 if the given subreddit is invalid; else the number
+    of subscribers is returned
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "api-practice (by /u/madu-foro)"}
+
+    reddit_response = requests.get(url, headers=headers).json()
+
+    subscribers = reddit_response.get("data", {}).get("subscribers", 0)
+    return subscribers
